f98b74b23bcc5fac2b8fa1dc76191b48d2465125
zerakjamil <135851996+zerakjamil@users.noreply.github.com>
2025-01-25 00:15:23 +0300

    Add homepage template with sliders and sections
    Introduced the homepage template using Blade, featuring a hero slider, category carousel, and hot deals section. Includes support for dynamic animations and responsive Swiper.js configurations for product carousels.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
