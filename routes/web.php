d7a5950172fe5a40af2a42a3c891586af64343ae
zerakjamil <135851996+zerakjamil@users.noreply.github.com>
2025-01-26 21:58:41 +0300

    Add role-based dashboards and refactor routes/links
    Implemented role-based navigation with dashboards for Admin and User roles. Updated routes, middleware, and controllers to handle role-specific logic. Replaced hardcoded links with named routes and removed unused language switcher form.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -9,11 +12,23 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
+
 Route::get('/greeting/{locale}', function(string $locale){
+
     if (!in_array($locale, ['en', 'ar', 'kr'])) {
         abort(400);
     }
+
     session(['locale' => $locale]);
     App::setLocale($locale);
+
     return redirect()->back();
+
 })->name('language.switch');
