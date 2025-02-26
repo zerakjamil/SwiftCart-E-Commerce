b9720a7eaa37629bcc02227bd60b65550a669097
zerakjamil <135851996+zerakjamil@users.noreply.github.com>
2025-02-27 00:28:12 +0300

    Enhance coupon management: create coupons table with necessary fields and constraints

---
+++ database/migrations/2025_02_26_184114_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique()->index();
+            $table->string('description')->nullable();
+            $table->enum('type', ['fixed', 'percent']);
+            $table->decimal('value', 10, 2);
+            $table->decimal('cart_value', 10, 2)->nullable();
+            $table->date('expiry_date');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
